9e8a1fe70724da7eacd5d35809ae498dbceb849b
Leonidasacosta <89566738+Leonidasacosta@users.noreply.github.com>
2021-09-25 18:50:34 -0400

    Completed Video 5.3 UpdatedToDoList!

--- ToDo List/ToDoListViewController.swift
+++ ToDo List/ToDoListViewController.swift
@@ -17,6 +17,7 @@ class ToDoListViewController: UIViewController {
         
         tableView.delegate = self
         tableView.dataSource = self
+        
     }
 }
 extension ToDoListViewController: UITableViewDelegate, UITableViewDataSource {

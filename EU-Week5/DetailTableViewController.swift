5a1b09e66cf55e6781b48332a3c504fc2078e06c
Leonidasacosta <89566738+Leonidasacosta@users.noreply.github.com>
2021-09-25 18:22:44 -0400

    Completed Video 5.3!

---
+++ EU-Week5/DetailTableViewController.swift
@@ -0,0 +1,28 @@
+//
+//  DetailTableViewController.swift
+//  EU-Week5
+//
+//  Created by Leonidas Acosta on 9/25/21.
+//
+
+import UIKit
+
+class DetailTableViewController: UITableViewController {
+    @IBOutlet weak var saveBarButton: UIBarButtonItem!
+    @IBOutlet weak var countryField: UITextField!
+    @IBOutlet weak var capitalField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
+        let isPresentingInAddMode = presentingViewController is UINavigationController
+        if isPresentingInAddMode {
+            dismiss(animated: true, completion: nil)
+        } else {
+            navigationController?.popViewController(animated: true)
+        }
+    }
+    
+}
